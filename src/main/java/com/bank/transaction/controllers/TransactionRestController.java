192cc0d1cfa3bc1b0cd323a582187968ad26624c
Adrian Norvic Melgarejo Siguas <amelgars@emeal.nttdata.com>
2022-08-08 16:43:31 -0500

    added log and new port

--- src/main/java/com/bank/transaction/controllers/TransactionRestController.java
+++ src/main/java/com/bank/transaction/controllers/TransactionRestController.java
@@ -22,57 +22,63 @@ public class TransactionRestController
     @GetMapping
     public Mono<ResponseEntity<Object>> findAll()
     {
+        log.info("[INI] findAll Transaction");
         return dao.findAll()
                 .doOnNext(transaction -> log.info(transaction.toString()))
                 .collectList()
                 .map(transactions -> ResponseHandler.response("Done", HttpStatus.OK, transactions))
-                .onErrorResume(error -> Mono.just(ResponseHandler.response(error.getMessage(), HttpStatus.BAD_REQUEST, null)));
+                .onErrorResume(error -> Mono.just(ResponseHandler.response(error.getMessage(), HttpStatus.BAD_REQUEST, null)))
+                .doFinally(fin -> log.info("[END] findAll Transaction"));
     }
 
     @GetMapping("/{id}")
     public Mono<ResponseEntity<Object>> find(@PathVariable String id)
     {
+        log.info("[INI] find Transaction");
         return dao.findById(id)
                 .doOnNext(transaction -> log.info(transaction.toString()))
                 .map(transaction -> ResponseHandler.response("Done", HttpStatus.OK, transaction))
-                .onErrorResume(error -> Mono.just(ResponseHandler.response(error.getMessage(), HttpStatus.BAD_REQUEST, null)));
+                .onErrorResume(error -> Mono.just(ResponseHandler.response(error.getMessage(), HttpStatus.BAD_REQUEST, null)))
+                .doFinally(fin -> log.info("[END] find Transaction"));
     }
 
     @PostMapping
     public Mono<ResponseEntity<Object>> create(@RequestBody Transaction tran)
     {
-
+        log.info("[INI] create Transaction");
         return dao.save(tran)
                 .doOnNext(transaction -> log.info(transaction.toString()))
                 .map(transaction -> ResponseHandler.response("Done", HttpStatus.OK, transaction)                )
-                .onErrorResume(error -> Mono.just(ResponseHandler.response(error.getMessage(), HttpStatus.BAD_REQUEST, null)));
+                .onErrorResume(error -> Mono.just(ResponseHandler.response(error.getMessage(), HttpStatus.BAD_REQUEST, null)))
+                .doFinally(fin -> log.info("[END] create Transaction"));
     }
 
     @PutMapping("/{id}")
     public Mono<ResponseEntity<Object>> update(@PathVariable("id") String id, @RequestBody Transaction act)
     {
+        log.info("[INI] update Transaction");
         return dao.existsById(id).flatMap(check -> {
             if (check)
                 return dao.save(act)
                         .doOnNext(transaction -> log.info(transaction.toString()))
                         .map(transaction -> ResponseHandler.response("Done", HttpStatus.OK, transaction)                )
                         .onErrorResume(error -> Mono.just(ResponseHandler.response(error.getMessage(), HttpStatus.BAD_REQUEST, null)));
+
             else
                 return Mono.just(ResponseHandler.response("Not found", HttpStatus.NOT_FOUND, null));
 
-        });
+        }).doFinally(fin -> log.info("[END] update Transaction"));
     }
 
     @DeleteMapping("/{id}")
     public Mono<ResponseEntity<Object>> delete(@PathVariable("id") String id)
     {
-        log.info(id);
-
+        log.info("[INI] delete Transaction");
         return dao.existsById(id).flatMap(check -> {
             if (check)
                 return dao.deleteById(id).then(Mono.just(ResponseHandler.response("Done", HttpStatus.OK, null)));
             else
                 return Mono.just(ResponseHandler.response("Not found", HttpStatus.NOT_FOUND, null));
-        });
+        }).doFinally(fin -> log.info("[END] delete Transaction"));
     }
 }
